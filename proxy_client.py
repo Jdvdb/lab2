24450b8bc3790a36edbfda7916dfe3ff0f7a9dfe
Jordan <jdvandenbruel@gmail.com>
2023-01-16 18:21:11 -0700

    Jk still have to do forking

--- proxy_client.py
+++ proxy_client.py
@@ -6,7 +6,7 @@ BYTES_TO_READ = 4096
 
 
 def get(host, port):
-    request = b"GET / HTTP/1.1\nwww.google.com: " + \
+    request = b"GET / HTTP/1.1\nHost: www.google.com: " + \
         host.encode("utf-8") + b"\n\n"
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
